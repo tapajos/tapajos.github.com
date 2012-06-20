6523c55fc55606c0a59010b13b19d293d515ad37
Marcos Tapajós <tapajos@gmail.com>
2012-06-19 21:55:52 -0300

    Inital commit

---
+++ script.js
@@ -0,0 +1,16 @@
+var tags = ["header", "footer"];
+for (var i = 0, tag; tag = tags[i]; i++) {
+	document.createElement(tag);
+};
+
+var _gauges = _gauges || [];
+(function() {
+  var t   = document.createElement('script');
+  t.type  = 'text/javascript';
+  t.async = true;
+  t.id    = 'gauges-tracker';
+  t.setAttribute('data-site-id', '4e525169613f5d1ce2000003');
+  t.src = '//secure.gaug.es/track.js';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(t, s);
+})();
